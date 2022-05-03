d9148bfdcb4c742da4c94419126d2d37fb75d862
rameshmane7218 <rameshmane7218@gmail.com>
2022-05-04 04:14:32 +0530

    added some content of homepage

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,14 @@
 // header part starts here 
 
-import { navbar } from "../components/navbar.js";
-document.getElementById("fixedHeader").innerHTML = navbar();
+// import { navbar } from "../components/navbar.js";
+// document.getElementById("fixedHeader").innerHTML = navbar();
 
 // header part end here 
 
+// // Page Content expand buttom for mobile device 
 
+// let expandData = () => {
+//     document.getElementsByClassName("secPageDataWrap").style.color = "red";
+// }
+// document.getElementById("expandBtn").addEventListener("click", expandData);
+// document.getElementsByClassName("secPageDataWrap").style.color = "red";
