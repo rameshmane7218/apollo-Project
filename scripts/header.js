69db3ffeacf00a61e0f4e7f398b4664223c194b2
rameshmane7218 <rameshmane7218@gmail.com>
2022-05-04 03:13:03 +0530

    added footer part

--- scripts/header.js
+++ scripts/header.js
@@ -3,3 +3,80 @@
 
 console.log("hiii");
 
+// mobile Navigation part 
+
+// localStorage.setItem("login","false");
+localStorage.setItem("login","true");
+let loginStatus = localStorage.getItem("login");
+// console.log(loginStatus);
+
+if(loginStatus == "true"){
+    // document.getElementById("navigation").innerHTML = null;
+    document.getElementById("mobNavigation").innerHTML =` 
+    <a class="mobNavigationBtn mobNavigationBtn-action mobNavigationBtn-base"  href="index.html">
+        <span class="mobNavigationIcon">
+            <i class="fa-regular fa-calendar-plus"></i>
+            
+            <span class="mobNavigationLable">Appintment</span>
+        </span>
+    </a>
+    <a class="mobNavigationBtn mobNavigationBtn-action mobNavigationBtn-base"  href="#">
+        <span class="mobNavigationIcon">
+            <i class="fa-solid fa-pills"></i>
+            <span class="mobNavigationLable">Pharmacy</span>
+        </span>
+    </a>
+    <a class="mobNavigationBtn mobNavigationBtn-action mobNavigationBtn-base"  href="#">
+        <span class="mobNavigationIcon">
+            <i class="fa-solid fa-microscope"></i>
+            <span class="mobNavigationLable">Lab Tests</span>
+        </span>
+    </a>
+    <a class="mobNavigationBtn mobNavigationBtn-action mobNavigationBtn-base"  href="#">
+        <span class="mobNavigationIcon">
+       
+            <i class="fa-solid fa-clipboard-user"></i>
+            <span class="mobNavigationLable">Health Records</span>
+        </span>
+    </a>
+    <a class="mobNavigationBtn mobNavigationBtn-action mobNavigationBtn-base"  href="#">
+        <span class="mobNavigationIcon">
+            <i class="fa-solid fa-user"></i>
+            <span class="mobNavigationLable">My Account</span>
+        </span>
+    </a>`
+}else{
+    // document.getElementById("navigation").innerHTML = null;
+    document.getElementById("mobNavigation").innerHTML =`
+    <a class="mobNavigationBtn mobNavigationBtn-action mobNavigationBtn-base"  href="index.html">
+        <span class="mobNavigationIcon">
+            <i class="fa-solid fa-house"></i>
+        
+            <span class="mobNavigationLable">Home</span>
+        </span>
+    </a>
+    <a class="mobNavigationBtn mobNavigationBtn-action mobNavigationBtn-base"  href="#">
+        <span class="mobNavigationIcon">
+            <i class="fa-solid fa-user-doctor"></i>
+            <span class="mobNavigationLable">Doctors</span>
+        </span>
+    </a>
+    <a class="mobNavigationBtn mobNavigationBtn-action mobNavigationBtn-base"  href="#">
+        <span class="mobNavigationIcon">
+            <i class="fa-solid fa-pills"></i>
+            <span class="mobNavigationLable">Pharmacy</span>
+        </span>
+    </a>
+    <a class="mobNavigationBtn mobNavigationBtn-action mobNavigationBtn-base"  href="#">
+        <span class="mobNavigationIcon">
+            <i class="fa-solid fa-microscope"></i>
+            <span class="mobNavigationLable">Lab Tests</span>
+        </span>
+    </a>
+    <a class="mobNavigationBtn mobNavigationBtn-action mobNavigationBtn-base"  href="#">
+        <span class="mobNavigationIcon">
+            <i class="fa-solid fa-viruses"></i>
+            <span class="mobNavigationLable">Covid-19</span>
+        </span>
+    </a>`;
+};
